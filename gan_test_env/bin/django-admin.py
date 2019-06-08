d5787db28acefe847f7c687e511fa6c4295db5b7
kimoto <japanworldconect@yahoo.co.jp>
2019-06-08 21:02:07 +0900

    Django環境作成

---
+++ gan_test_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kimoto/Desktop/python_workspace/style_gan_test/gan_test_env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
