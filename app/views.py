b29bb8965e56465c8dac59764405acc1f0cef396
kimoto <japanworldconect@yahoo.co.jp>
2019-06-11 01:43:04 +0900

    index view create

--- app/views.py
+++ app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+def index(request):
+    if 'name' in request.GET:
+        name = request.GET['name']
+        result = 'my name is ' + name + '!!'
+    else:
+        result = 'please input query name'
+    return HttpResponse(result)
+
+class IndexTemplateView(TemplateView):
+    template_name = "index.html"
