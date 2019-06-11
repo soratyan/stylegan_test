fe591bc89d656f373c8532034a22728a02006389
iMac0763 <imac0763@iMac0763noiMac.local>
2019-06-11 19:49:02 +0900

    templateファイルの作成

--- app/views.py
+++ app/views.py
@@ -5,13 +5,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
+# def index(request):
+#     if 'name' in request.GET:
+#         name = request.GET['name']
+#         result = 'my name is ' + name + '!!'
+#     else:
+#         result = 'please input query name'
+#     return HttpResponse(result)
+
 def index(request):
-    if 'name' in request.GET:
-        name = request.GET['name']
-        result = 'my name is ' + name + '!!'
-    else:
-        result = 'please input query name'
-    return HttpResponse(result)
+    #query paramater
+    params = {
+    'title':'hello world',
+    'msg':'こんにちは',
+    'gopage':'secondindex'
+    }
+    return render(request,'app1/index.html', params)
+
+def second(request):
+    params = {
+    'title':'hello world',
+    'msg':'こんにちは',
+    'gopage':'index'
+    }
+    return render(request,'app1/index.html', params)
 
 class IndexTemplateView(TemplateView):
     template_name = "index.html"
